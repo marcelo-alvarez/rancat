bd28adbb9dd4f96650ea3de1a4b13631f15c27bd
Marcelo Alvarez <marcelo.alvarez@berkeley.edu>
2021-06-16 18:07:48 -0700

    setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='rancat',
+      version='0.1',
+      description='Random halo catalogs',
+      url='http://github.com/marcelo-alvarez/rancat',
+      author='Marcelo Alvarez',
+      license='MIT',
+      packages=['rancat'],
+      zip_safe=False)
